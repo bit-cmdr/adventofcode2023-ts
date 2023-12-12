f0b6eac99cc2220eb36fa4fab323a5e3c260e539
Alexander Kahoun <alexander.kahoun@outlook.com>
2023-12-12 07:21:06 -0700

    feat: day8

---
+++ src/day8/puzzle1.ts
@@ -0,0 +1,51 @@
+import * as fs from 'node:fs';
+
+type Node = { [key: string]: [string, string] };
+
+function parseLine(line: string): [string, string, string] {
+  const [value, left, right] = line.match(/\w+/g)!.map((s) => s.trim());
+  return [value, left, right];
+}
+
+function buildMap(lines: string[]): [string[], Node] {
+  const traversal = lines[0].split('');
+
+  const map: Node = {};
+  for (let i = 2; i < lines.length; i++) {
+    const [value, left, right] = parseLine(lines[i]);
+    map[value] = [left, right];
+  }
+
+  return [traversal, map];
+}
+
+function traverse(map: Node, method: string[]): number {
+  let current = 'AAA';
+  let step = 0;
+  let visitor = 0;
+  while (current !== 'ZZZ') {
+    const [left, right] = map[current];
+    step++;
+    current = method[visitor] === 'L' ? left : right;
+    visitor = visitor === method.length - 1 ? 0 : visitor + 1;
+  }
+  return step;
+}
+
+function puzzleInput(): string {
+  return fs.readFileSync('src/day8/input.txt', 'utf8');
+}
+
+function main(): void {
+  const lines = puzzleInput().split('\n');
+  console.log('Lines: ', lines);
+
+  const [traversal, map] = buildMap(lines);
+  console.log('Traversal: ', traversal);
+  console.log('Map: ', JSON.stringify(map, null, 2));
+
+  const steps = traverse(map, traversal);
+  console.log('Steps: ', steps);
+}
+
+main();
